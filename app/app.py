6dbc99bc61f1c97a5ad9b9eed90227a29ab309be
Junior.Sirivadhna <ksirivad@bu.edu>
2019-02-09 15:51:46 -0500

    spotipy working and have some ui

--- app/app.py
+++ app/app.py
@@ -3,8 +3,8 @@ import sys
 import os
 import json
 import webbrowser
-import spotipy.util as util
 import spotipy
+import spotipy.util as util
 from json.decoder import JSONDecoder
 
 from flask import Flask, Response, request, render_template, redirect, url_for
@@ -16,34 +16,50 @@ app = Flask(__name__)
 # login_manager = flask_login.LoginManager()
 # login_manager.init_app(app)
 
-CLIENT_ID = '36feecb46ca04c2f987a737e6eab3477'
-CLIENT_SECRET = '757ac79156a74c95a7f5a0af8142c77a'
-redirect_uri = 'http://google.com'
-scope = 'user-read-currently-playing'
 
-username = "kamoltat"
-#userId: https://open.spotify.com/user/kamoltat?si=zwwxCHVLSfKzCJIm8CXXtA
 
-# user = spotifyObject.current_user()
-# print(json.dumps(user,sort_keys=True,indent=4))
-try:
-    token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, redirect_uri)
-except:
-    print("except hits")
-    # os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username)
+redirect_uri = "https://google.com/"
+scope = "user-library-read"
+client_id="36feecb46ca04c2f987a737e6eab3477"
+client_secret="757ac79156a74c95a7f5a0af8142c77a"
+username = 'kamoltat'
+# birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+#https://open.spotify.com/user/kamoltat?si=5Us0ZgjUSVGYBHCagQi7UA
 
+
+
+
+
+
+token = util.prompt_for_user_token(username,scope,client_id,client_secret,redirect_uri)
 sp = spotipy.Spotify(auth=token)
+user = sp.current_user()
+
+displayName = user['display_name']
+followers = user['followers']['total']
+playlists = sp.user_playlists(username)
+
+
+list1 = []
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        list1.append((playlist['uri'],playlist['name']))
+        print("%4d %s %s" % (i + 1 , playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
+
+
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('hello.html',title="Music Rating",items=list1)
 
 
 @app.route('/drop_down')
 def drop_down():
-    return ''
 
-    return render_template('top_users.html', message='TopUsers',topUsers = topUsers)
+    return render_template('drop_down.html')
 
 if __name__ == '__main__':
 
